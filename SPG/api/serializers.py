324a9c1fb2c00d65035db3a1aa48ab8062b9d09c
cyang258 <canadachunyang@gmail.com>
2023-12-28 12:40:26 -0500

    Create frontend part

--- SPG/api/serializers.py
+++ SPG/api/serializers.py
@@ -5,4 +5,8 @@ class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip')
-        
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')
