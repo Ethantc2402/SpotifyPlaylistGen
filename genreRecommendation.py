b1745fc8afa0b94abfd40ab4d68eb6ccc2c0aa69
echao <echao8@uwo.ca>
2024-01-11 14:01:56 -0500

    Tempo and genre recommendation on spotify api working

--- genreRecommendation.py
+++ genreRecommendation.py
@@ -1,110 +1,33 @@
-# import pandas as pd
-# import spotipy
-# from spotipy.oauth2 import SpotifyClientCredentials
-
-# # Pretty much just gets the Top 10 most poplular songs in the same genre as 
-# # the inputted song name
-
-# # function to get the Spotify URL's for songs that are recommended based on genre
-# def get_spotify_url(track_name, artist_name, sp):
-#     query = f"{track_name} {artist_name}"
-#     # querying the Spotify API to search for the song
-#     results = sp.search(q=query, type='track', limit=1)
-
-#     # if the song was found, return the Spotify URL
-#     if results['tracks']['items']:
-#         return results['tracks']['items'][0]['external_urls']['spotify']
-#     else:
-#         return None
-
-# def recommendByGenre(song_name, data, sp):
-#     # filter data based on the input song name
-#     # changing the case of the song name to lowercase for easier comparison
-#     song_data = data[data['track_name'].str.lower() == song_name.lower()]
-
-#     if song_data.empty:
-#         # song was not found in the dataset
-#         print(f"No match found for the song '{song_name}'.")
-#         return
-
-#     # extract the genre of the inputted song from the csv data
-#     # returning a view of the selected row and column
-#     input_genre = song_data.iloc[0]['genre']
-
-#     # filter data based on the genre of the input song
-#     # gives list of just the songs in the same genre as the input song
-#     genre_matches = data[data['genre'] == input_genre]
-
-#     # remove the actual inputted song from the list of matches
-#     # making sure the song name is in lowercase for easier comparison
-#     # and that there is no duplicate/repeated song names in the list
-#     genre_matches = genre_matches[genre_matches['track_name'].str.lower() != song_name.lower()]
-
-#     # sorting the top 10 matches by popularity (descending order)
-#     top_recommendations = genre_matches.sort_values(by='popularity', ascending=False).head(10)
-
-#     # add a column for Spotify URLs
-#     top_recommendations['spotify_url'] = top_recommendations.apply(lambda row: get_spotify_url(row['track_name'], row['artist_name'], sp), axis=1)
-
-#     # return the top 10 recommendation list
-#     return top_recommendations
-
-# # Only run the following code if this file is being run directly
-# # to be later exported as a module to be used in other files
-# # once the full filtering/recommendation process for songs is completed
-# if __name__ == "__main__":
-#     # load CSV data into a DataFrame using pandas library
-#     spotify_data = pd.read_csv('SpotifyPlaylistGen\spotify_data.csv')
-
-#     # take user input for the song name
-#     user_input = input("Enter the name of a song from spotify: ")
-
-#     # set up Spotify API credentials
-#     # to set up your own credentials, go to https://developer.spotify.com/dashboard/
-#     # get your id and secret and replace the values below
-#     client_id = 'cf9550faad1b4d2f93b868511b7bebf2'
-#     client_secret = 'adc6e3b265824a4d8bcaee3da0a652e5'
-#     client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-#     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-#     # get recommendations based on genre
-#     recommendations = recommendByGenre(user_input, spotify_data, sp)
-
-#     # display recommendations including Spotify links
-#     if recommendations is not None:
-#         print("\nTop 10 Recommendations Based on Genre:")
-#         for index, row in recommendations[['artist_name', 'track_name', 'popularity', 'genre', 'spotify_url']].iterrows():
-#             print(f"{row['track_name']} by {row['artist_name']} ({row['popularity']} popularity, Genre: {row['genre']})")
-#             print(f"Spotify Link: {row['spotify_url']}")
-#             print()
-
-
-import pandas as pd
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 # function to get the Spotify URL's for songs that are recommended based on genre
-def get_spotify_url(track_id, sp):
+def get_spotify_info(track_id, sp):
     track = sp.track(track_id)
-    return track['external_urls']['spotify']
+    audio_features = sp.audio_features([track_id])[0] # Retrieve audio features of the inputted song to get a relative tempo from
+    return track, audio_features
 
-def recommendByGenre(song_name, sp):
-    # querying the Spotify API to search for the input song
+
+def recommendByGenre_Tempo(song_name, sp):
+    # Querying the Spotify API to search for the input song
     results = sp.search(q=song_name, type='track', limit=1)
 
-    # if the song was found, proceed with recommendations
+    # If the song was found, proceed with recommendations
     if results['tracks']['items']:
         input_track = results['tracks']['items'][0]
         input_genre = input_track['artists'][0]['id']  # Assume the genre is based on the first artist of the track
 
-        # get top tracks in the same genre as the input song
-        recommendations = sp.recommendations(seed_artists=[input_genre], limit=10)
+        # Get tempo of the input track
+        _, input_audio_features = get_spotify_info(input_track['id'], sp)
+        input_tempo = input_audio_features['tempo']
 
-        print(recommendations)
+        print("Tempo of the song inputted: " + str(input_tempo) + " BPM")
 
-        # add a column for Spotify URLs
-        recommendations['spotify_url'] = [get_spotify_url(track['id'], sp) for track in recommendations['tracks']]
+        # Get top tracks in the same genre as the input song with a target tempo
+        recommendations = sp.recommendations(seed_artists=[input_genre], target_tempo=[input_tempo], limit=10)
 
+        # Add a column for Spotify URLs
+        recommendations['spotify_url'] = [get_spotify_info(track['id'], sp)[0]['external_urls']['spotify'] for track in recommendations['tracks']]
 
         return recommendations
 
@@ -112,24 +35,23 @@ def recommendByGenre(song_name, sp):
         print(f"No match found for the song '{song_name}'.")
         return None
 
+
 if __name__ == "__main__":
-    # take user input for the song name
+    # Take user input for the song name
     user_input = input("Enter the name of a song from Spotify: ")
 
-    # set up Spotify API credentials
-    # to set up your own credentials, go to https://developer.spotify.com/dashboard/
-    # get your id and secret and replace the values below
+    # Set up Spotify API credentials
     client_id = 'cf9550faad1b4d2f93b868511b7bebf2'
     client_secret = 'adc6e3b265824a4d8bcaee3da0a652e5'
     client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-    # get recommendations based on genre
-    recommendations = recommendByGenre(user_input, sp)
+    # Get recommendations based on genre and target tempo
+    recommendations = recommendByGenre_Tempo(user_input, sp)
 
-    # display recommendations including Spotify links
+    # Display recommendations including Spotify links
     if recommendations is not None:
-        print("\nTop 10 Recommendations Based on Genre:")
+        print("\nTop 10 Recommendations Based on Genre and Target Tempo:")
         for track in recommendations['tracks']:
             artists = ', '.join([artist['name'] for artist in track['artists']])
             print(f"{track['name']} by {artists} (Popularity: {track['popularity']}, Artist Name: {track['artists'][0]['name']})")
